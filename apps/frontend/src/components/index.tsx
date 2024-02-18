3964bb6c26d0be5092306b9758a94a36746302bb
zachalam <zach.alam@ey.com>
2024-02-17 19:21:39 -0800

    feat: add eth balance lookup and update layout

--- apps/frontend/src/components/index.tsx
+++ apps/frontend/src/components/index.tsx
@@ -11,7 +11,11 @@ import { Noir } from '@noir-lang/noir_js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { ProofData } from '@noir-lang/types';
 import { compile, PathToFileSourceMap } from '@noir-lang/noir_wasm';
+import ethers from 'ethers';
+// @ts-ignore
+import axios from 'axios';
 
+const EXCHANGE_RATE = 2700;
 
 async function getCircuit(name: string) {
   const res = await fetch(new URL('../circuits/src/main.nr', import.meta.url));
@@ -24,30 +28,54 @@ async function getCircuit(name: string) {
 }
 
 function Component() {
-  const [input, setInput] = useState({ verifiedAmount: 0, loanAmount: 0, downpaymentPercent: 0 });
+  const [input, setInput] = useState({ verifiedAmount: 0, loanAmount: 0, downpaymentPercent: 0, ethAddress: '' });
   const [proof, setProof] = useState<ProofData>();
+  const [viewableProof, setViewableProof] = useState<String>();
   const [noir, setNoir] = useState<Noir | null>(null);
   // const [backend, setBackend] = useState<BarretenbergBackend | null>(null);
-
+  const [addressBalance, setAddressBalance ] = useState<number>();
+  
   // Handles input state
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (e.target) setInput({ ...input, [e.target.name]: e.target.value });
   };
+  
 
   // Calculates proof
   const calculateProof = async () => {
     const calc = new Promise(async (resolve, reject) => {
-      const { proof, publicInputs } = await noir!.generateFinalProof(input);
-      console.log('Proof created: ', proof);
-      console.log('Public input: ', publicInputs);
-      setProof({ proof, publicInputs });
-      resolve(proof);
+      try {
+        const { proof, publicInputs } = await noir!.generateFinalProof(input);
+        console.log('Proof created: ', proof);
+        // proof -> create a new generic TX with proof as calldata
+        // init wagmi connector with metamask
+        // create and bundle tx with proof as calldata
+        // prompt user to sign / submit tx
+        // take txn -> send to public node / infura
+        // call toast.promise() with txn hash
+        console.log('Public input: ', publicInputs);
+
+        setProof({ proof, publicInputs });
+
+        //const arrayBuffer = new Uint8Array([104, 101, 108, 108, 111]);
+        //const base64String = btoa(String.fromCharCode.apply(null, proof));
+        //@ts-ignore
+        const viewableProof = btoa(String.fromCharCode(...new Uint8Array(proof)));     
+        console.log(viewableProof);
+
+        setViewableProof(viewableProof);
+
+        resolve(proof);
+      } catch(e) {
+        reject(e);
+      }
+
     });
     toast.promise(calc, {
-      pending: 'Calculating proof...',
-      success: 'Proof calculated!',
-      error: 'Error calculating proof',
+      pending: 'Calculating CreditVerifier Proof...',
+      success: 'CreditVerifier Proof calculated!',
+      error: 'Error generating CreditVerifier proof; is your ETH balance high enough to qualify for this loan? ',
     });
   };
 
@@ -58,26 +86,38 @@ function Component() {
           proof: proof.proof,
           publicInputs: proof.publicInputs,
         });
-        console.log('Proof verified: ', verification);
+        console.log('CreditVerifier Proof Verified: ', verification);
         resolve(verification);
       }
     });
 
     toast.promise(verifyOffChain, {
-      pending: 'Verifying proof off-chain...',
-      success: 'Proof verified off-chain!',
-      error: 'Error verifying proof',
+      pending: 'Verifying CreditVerifier proof off-chain...',
+      success: 'CreditVerifier Proof verified off-chain!',
+      error: 'Error verifying CreditVerifier proof',
     });
   };
 
-  // useEffect(() => {
-  //   if (proof) {
-  //     verifyProof();
-  //     return () => {
-  //       backend!.destroy();
-  //     };
-  //   }
-  // }, [proof]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${input.ethAddress}&tag=latest&apikey=4I4P85YQ8F97AH899JUKVFVKCKN923UTEC`);
+        const data = await response.json();
+        const ethInWei = data.result;
+        const eth =  ethInWei / 1e18;
+        const usdValue = Math.round(eth * EXCHANGE_RATE);
+        setAddressBalance(usdValue); // Update state with fetched data
+        setInput({...input, verifiedAmount: usdValue});
+      } catch (error) {
+        //setError(error); // Handle error appropriately
+      }
+    };
+  
+    fetchData();
+  
+    // Dependency array to prevent unnecessary re-fetches
+  }, [input.ethAddress]);
 
   const initNoir = async () => {
     const circuit = await getCircuit('main');
@@ -102,13 +142,17 @@ function Component() {
 
   return (
     <div className="container">
-      <h1>Example starter</h1>
+      <img src="logo.jpeg" style={{width:'100px'}} />
       <h2>This circuit validates users credit for loan amount</h2>
-      <p>Try it!</p>
       {/* Generate Proof */}
       <div className="formInput">
+        <label htmlFor="ethAddress">Ethereum Address </label>
+        <input name="ethAddress" type={'string'} onChange={handleChange} value={input.ethAddress} />
+      </div>
+
+      <div className="formInput">
         <label htmlFor="verifiedAmount">Verified Amount (secret input) </label>
-        <input name="verifiedAmount" min="0" type={'number'} onChange={handleChange} value={input.verifiedAmount} />
+        <input name="verifiedAmount" min="0" type={'number'} value={addressBalance} disabled />
       </div>
       
       <div className="formInput">
@@ -122,11 +166,12 @@ function Component() {
       </div>
 
       <button className="formButton" onClick={calculateProof}>Calculate proof</button>
-
+      <br /><br />      <br /><br />      <br />
       {/* Display proof */}
-     <div className='proofData'>{proof?.proof}</div>
-     <div className='publicInput'>{proof?.publicInputs}</div>
+     <div className='proofData'>{viewableProof}</div><br /><br />
+     <div className='publicInputs'>{proof?.publicInputs}</div>
 
+      <br /><br />
      {/* Verify Proof */}
       <button className="formButton" onClick={verifyProof}>Verify proof</button>
     </div>
